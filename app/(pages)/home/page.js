26e9f788b7a5bcc8a6021ccb6edd195f918bdf0f
Jiseungmin <wltmdals0118@sunmoon.ac.kr>
2025-05-06 23:29:20 +0900

    first commit

--- app/(pages)/home/page.js
+++ app/(pages)/home/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is the first page of your project.</p>
+      <h1>W Next.js App</h1>
+      <p>This is the  your project.</p>
       <a
         href="https://nextjs.org"
         target="_blank"
